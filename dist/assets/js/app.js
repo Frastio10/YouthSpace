d0b3f9a8f379a61dc8c9cb4136e440864516a169
frastio10 <frastio.agustiyan@gmail.com>
2019-08-27 22:37:33 +0700

    v1.4

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,3 +1,7 @@
 $('.popup-trigger').hover(function(event) {
 	$(this).children('.popup-content').toggleClass('d-none');
 });
+
+$('.flags').hover(function(event) {
+	$(this).children('.popup-content').toggleClass('d-none');
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -8,6 +8,7 @@ mix.copy('resources/js/*.js', 'dist/assets/js/');
 
 mix.sass('resources/sass/style.scss', 'dist/assets/css')
    .sass('resources/sass/home.scss', 'dist/assets/css')
+   .sass('resources/sass/news.scss', 'dist/assets/css')
    .options({
     processCssUrls: false, 
     postCss: [
