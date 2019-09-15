abd2c01fe0839a874248441d91a67c3edbec64ee
frastio10 <frastio.agustiyan@gmail.com>
2019-09-15 13:29:39 +0700

    added stack slide v1

--- resources/js/3dslider.js
+++ resources/js/3dslider.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function(){
+	jQuery(document).ready(function(){
 			function detect_active(){
 	  			// get active
 	  			var get_active = $("#dp-slider .dp_item:first-child").data("class");
@@ -47,4 +47,15 @@ jQuery(document).ready(function(){
 
 	  			detect_active();
 	  		});
+
+	  		const changeImage = ()=>{
+
+	  			var total = $(".dp_item").length;
+	  			$("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
+	  			$.each($('.dp_item'), function (index, dp_item) {
+	  				$(dp_item).attr('data-position', index + 1);
+	  			});
+	  			detect_active();
+	  		}
+	  		setInterval(changeImage, 4000);
 	  	});
